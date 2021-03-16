0f1341dba30eabef1b3ad3e39e8ca0608886f569
mahto53 <mahtosuyash35@gmail.com>
2021-03-17 00:23:32 +0530

    burger-Builder-App-using-React-JS

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React from  'react';
+import classes from './Logo.css';
+
+import burgerLogo from '../../assets/images/original.png';
+
+const logo =(props) => (
+
+	<div className = {classes.Logo} style={{height:props.height}}>
+		<img src ={burgerLogo} alt ="MyBurger" /> 
+
+	</div>
+
+);
+
+
+
+export default logo;
---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Spinner.css';
+
+
+const spinner = () => (
+	<div classname = {classes.Loader}>Loading...</div>
+);
+
+
+export default spinner;
